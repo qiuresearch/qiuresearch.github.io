686c552b4225eb6a9b28fa9cddd226e648cf94d1
qiuresearch <xiangyun@gmail.com>
2025-12-16 07:35:32 +0000

    Deploying to gh-pages from @ qiuresearch/qiuresearch.github.io@12f0846506b0fe651c48ad7dff4c6f9d5aff8e18 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,7 +479,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Qiu_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
