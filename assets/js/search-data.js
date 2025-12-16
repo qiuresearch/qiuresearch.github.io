070e5f031598fef7cf3a22ffce1f639babf2470a
qiuresearch <xiangyun@gmail.com>
2025-12-16 07:28:56 +0000

    Deploying to gh-pages from @ qiuresearch/qiuresearch.github.io@df80b423d496a8e7a649d0d2777b151e57ee9dd6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -489,6 +489,13 @@ ninja.data = [{
           window.open("mailto:%78%71%69%75@%67%77%75.%65%64%75", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/qiuresearch", "_blank");
+        },
+      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
